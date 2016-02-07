e7b97044803fbca999702ea391c1067d2ca5876e
lucas persson <luben93@gmail.com>
2016-02-07 02:37:43 +0100

    genserver and supervisors

---
+++ kv/lib/kv/supervisor.ex
@@ -0,0 +1,31 @@
+
+# defmodule KV.Supervisor do
+#   use Supervisor
+
+#   # A simple module attribute that stores the supervisor name
+#   @name KV.Supervisor
+
+#   def start_link() do
+#     Supervisor.start_link(__MODULE__, :ok, name: @name)
+#   end
+# #
+#   def start_bucket do
+#     Supervisor.start_child(@name, [])
+#   end
+
+#   # def init(:ok) do
+#   #   children = [
+#   #     worker(KV.Bucket, [], restart: :temporary)
+#   #   ]
+
+#   #   supervise(children, strategy: :simple_one_for_one)
+#   # end
+#   def init(:ok) do
+#     children = [
+#       worker(KV.Registry, [KV.Registry]),
+#       supervisor(KV.Bucket.Supervisor, [])
+#     ]
+
+#     supervise(children, strategy: :one_for_one)
+#   end
+# end
