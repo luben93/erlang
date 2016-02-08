7099b476a1b1cc6a9a6edffdcd4098b92fd74c7f
lucas persson <luben93@gmail.com>
2016-02-08 01:56:02 +0100

    moar test

--- kv/test/client_test.exs
+++ kv/test/client_test.exs
@@ -7,17 +7,25 @@ defmodule KV.ClientTest do
     {:ok, client: client}
   end
 
-  test "spawns buckets",%{client: client} do
-    #{:ok,_pid} = KV.Client.start_link
-    assert KV.Client.read(client,"shopping") == {:error,:instance}
+  test "spawns client",%{client: client} do
+      #{:ok,_pid} = KV.Client.start_link
+      assert KV.Client.read(client,"shopping") == {:error,:instance}
 
-  #  KV.Client.create(registry, "shopping")
-   # assert {:ok, bucket} = KV.Client.lookup(registry, "shopping")
+    #  KV.Client.create(registry, "shopping")
+     # assert {:ok, bucket} = KV.Client.lookup(registry, "shopping")
 
-    KV.Client.write(client, "milk", 1)
-    assert KV.Client.read(client, "milk") == {:ok,1}
-    KV.Client.delete(client,"milk")
-    assert KV.Client.read(client, "milk") == {:error,:instance}
-  end
+      KV.Client.write(client, "milk", 1)
+      assert KV.Client.read(client, "milk") == {:ok,1}
+      KV.Client.delete(client,"milk")
+      assert KV.Client.read(client, "milk") == {:error,:instance}
+    end
+
+    test "spawn and stop client",%{client: client} do
+           KV.Client.write(client, "milk", 1)
+           assert KV.Client.read(client, "milk") == {:ok,1}
+           Agent.stop(client)
+    end
+
+
 end
 
