75fd61651ac5fc07ebcd139b3543537b7fc6a3de
luben <luben93@gmail.com>
2016-02-12 19:50:47 +0000

    done? testing and all

--- labb1/lib/labb1.ex
+++ labb1/lib/labb1.ex
@@ -5,34 +5,59 @@ defmodule Labb1 do
 
 	@doc"""
 	starts station
-	`ref` = unique id for station
+	`ref` = unique id for station (pid)
 	"""
 	def start_link(total,taken) do
-		#something return {:ok,ref}
+		GenServer.start_link(__MODULE__,{total,taken},[])
+	end
+
+	#quickstart for testing
+	def st do 
+		start_link(5,2)
 	end
 
 	def release_cycle(ref) do
 		#something return :ok | {:error,:empty}
+		GenServer.call(ref,{:take})
 	end
 
 	def secure_cycle(ref) do
 		#something return :ok | {:error,:full}
+		GenServer.call(ref,{:return})
 	end
 
 	def get_info(ref) do
-		#{:ok,[{:total,total},{:taken,taken},{:free,free}]}
+		GenServer.call(ref,{:info})
 	end
 
-	def init(:ok) do
-		#state
+	#server{myInt, _} = :string.to_integer(to_char_list("23"))
+
+	def init({total,taken}=state) do
+		{:ok,state}
 	end
 
-	def handle_call({:call,var},_from,state) do
-		:ok
+	def handle_call({:take},_from,{total,taken}=state) do
+		cond do
+			total == taken -> 
+				{:reply,{:error,:empty},state}
+			true ->
+				{:reply,:ok,{total,taken+1}}
+		end
 	end
 
-	def handle_cast({:cast,var},state) do
-		#no return
+	def handle_call({:return},_from,{total,taken}=state) do
+		cond do 
+			0 == taken ->
+				{:reply,{:error,:full},state}
+			true ->
+				{:reply,:ok,{total,taken-1}}
+
+		end
+	end
+
+
+	def handle_call({:info},_from,{total,taken}=state) do #? define ref in vars to match state?
+		{:reply,{:ok,[{:total,total},{:taken,taken},{:free,total - taken}]},state}
 	end
 
 end
--- labb1/test/labb1_test.exs
+++ labb1/test/labb1_test.exs
@@ -5,4 +5,44 @@ defmodule Labb1Test do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "start and get info" do
+  	{:ok,pid} = Labb1.start_link(5,2)
+  	assert {:ok, [total: 5, taken: 2, free: 3]} == Labb1.get_info(pid)
+  end
+
+  test "taken and return " do 
+  		{:ok,pid} = Labb1.start_link(5,2)
+  		{:ok, [total: 5, taken: 2, free: 3]} = Labb1.get_info(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		{:error,:empty}=Labb1.release_cycle(pid)
+  		:ok=Labb1.secure_cycle(pid)
+  		:ok=Labb1.secure_cycle(pid)
+   		:ok=Labb1.secure_cycle(pid)
+  		:ok=Labb1.secure_cycle(pid)
+  		:ok=Labb1.secure_cycle(pid)
+  		{:error,:full}=Labb1.secure_cycle(pid)
+  		assert {:ok, [total: 5, taken: 0, free: 5]} == Labb1.get_info(pid)		
+
+  end
+
+  test "multiple clients" do 
+  		{:ok,pid} = Labb1.start_link(5,2)
+  		{:ok, [total: 5, taken: 2, free: 3]} = Labb1.get_info(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		:ok=Labb1.release_cycle(pid)
+  		 {:error,:empty}=Labb1.release_cycle(pid)
+
+  		{:ok,pid2} = Labb1.start_link(10,4)
+  		{:ok, [total: 10, taken: 4, free: 6]} = Labb1.get_info(pid2)
+  		:ok=Labb1.release_cycle(pid2)
+
+   		 {:error,:empty}=Labb1.release_cycle(pid)
+
+  end
+
+
 end
