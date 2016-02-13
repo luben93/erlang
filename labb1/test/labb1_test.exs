2e9890c872c7c1f2a33430759ce9aa93bd48463c
luben <luben93@gmail.com>
2016-02-13 18:44:36 +0000

    fixed correct again

--- labb1/test/labb1_test.exs
+++ labb1/test/labb1_test.exs
@@ -8,13 +8,13 @@ defmodule Labb1Test do
 
   test "start and get info" do
   	{:ok,pid} = Labb1.start_link(5,2)
-  	assert {:ok, [total: 5, taken: 2, occupied: 3]} == Labb1.get_info(pid)
+  	assert {:ok, [total: 5, occupied: 2, free: 3]} == Labb1.get_info(pid)
     assert {:error,reason} = Labb1.start_link(-1,3)
  end
 
-  test "taken and return " do 
+  test "occupied and return " do 
   		{:ok,pid} = Labb1.start_link(5,2)
-  		{:ok, [total: 5, taken: 2, occupied: 3]} = Labb1.get_info(pid)
+  		{:ok, [total: 5, occupied: 2, free: 3]} = Labb1.get_info(pid)
   		:ok=Labb1.release_cycle(pid)
   		:ok=Labb1.release_cycle(pid)
   		:ok=Labb1.release_cycle(pid)
@@ -25,20 +25,20 @@ defmodule Labb1Test do
   		:ok=Labb1.secure_cycle(pid)
   		:ok=Labb1.secure_cycle(pid)
   		{:error,:full}=Labb1.secure_cycle(pid)
-  		assert {:ok, [total: 5, taken: 0, occupied: 5]} == Labb1.get_info(pid)		
+  		assert {:ok, [total: 5, occupied: 0, free: 5]} == Labb1.get_info(pid)		
 
   end
 
   test "multiple clients" do 
   		{:ok,pid} = Labb1.start_link(5,2)
-  		{:ok, [total: 5, taken: 2, occupied: 3]} = Labb1.get_info(pid)
+  		{:ok, [total: 5, occupied: 2, free: 3]} = Labb1.get_info(pid)
   		:ok=Labb1.release_cycle(pid)
   		:ok=Labb1.release_cycle(pid)
   		:ok=Labb1.release_cycle(pid)
   		 {:error,:empty}=Labb1.release_cycle(pid)
 
   		{:ok,pid2} = Labb1.start_link(10,4)
-  		{:ok, [total: 10, taken: 4, occupied: 6]} = Labb1.get_info(pid2)
+  		{:ok, [total: 10, occupied: 4, free: 6]} = Labb1.get_info(pid2)
   		:ok=Labb1.release_cycle(pid2)
 
    		 {:error,:empty}=Labb1.release_cycle(pid)
